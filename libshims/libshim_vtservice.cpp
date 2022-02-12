8cb36e5f37f40acd4a07e7abc7a25ff14de03d20
officialputuid <officialputuid@hack.id>
2022-02-12 13:04:55 +0800

    RM6785: libshims: Added missing symbols vtservice
    Co-authored-by: SamarV-121 <samarvispute121@pm.me>
    Co-authored-by: R0rt1z2 <rogerortizleal@gmail.com>
    Co-authored-by: Kyle Harrison <khwebmail@gmail.com>
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
    Change-Id: I109db7220b47e2d6b174bfd176edf6e7449324aa

--- libshims/libshim_vtservice.cpp
+++ libshims/libshim_vtservice.cpp
@@ -37,5 +37,9 @@ void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb(
         _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(thisptr, bufferProducer, controlledByApp, nullptr);
 }
 
+void _ZN7android11AudioSourceC1E14audio_source_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(audio_source_t inputSource, const String16 &opPackageName,
+    uint32_t sampleRate, uint32_t channels, uint32_t outSampleRate, uid_t uid, pid_t pid,
+    audio_port_handle_t selectedDeviceId, int32_t selectedMicDirection, float selectedMicFieldDimension);
+
 int _ZN7android10MediaMuxerC1EiNS0_12OutputFormatE[6] = { 0, 1, 2, 3, 4, NULL };
 }
