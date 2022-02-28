f12b87784e7591dfe5402e213048f590d433827e
ImFlynnn <ahmadzaid13.az@gmail.com>
2022-02-28 09:45:37 +0000

    Revert "RM6785: Use Pixel power-off charging animation"
    This reverts commit 1f513dbd499fc48866458b1aef8f3e7ea6f830ae.

--- device.mk
+++ device.mk
@@ -238,11 +238,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Pixel Charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    product_charger_res_images
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-V1-ndk_platform.vendor \
